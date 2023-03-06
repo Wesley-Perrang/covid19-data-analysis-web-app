bfb709b39e3cc80d905ddc079f02a6c1c4d98aec
weperra022 <weperra022@student.wethinkcode.co.za>
2023-03-06 17:05:50 +0200

    updated file

--- web_scaper_to_CSV_file.py
+++ web_scaper_to_CSV_file.py
@@ -1,32 +1,35 @@
-import requests
-import csv
-import json
+def gets_south_african_covid19_data():
+    """"""
+    import requests
+    import csv
+    import json
+    import datetime
 
-url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+    url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 
-# send request to download the file
-response = requests.get(url)
+    # Send request to download the file
+    response = requests.get(url)
 
-# read content of the downloaded file
-content = response.content.decode('utf-8')
+    # This will decode the content to strings 
+    content = response.content.decode('utf-8')
+    
+    list_content = None
 
-# parse the CSV data
-rows = csv.reader(content.splitlines())
-header = next(rows)
-data = {}
+    # parse the CSV data
+    rows = csv.reader(content.splitlines())
 
-for row in rows:
-    country = row[1]
-    if country not in data:
-        data[country] = {}
-    for i in range(4, len(header)):
-        date = header[i]
-        confirmed = int(row[i])
-        if date not in data[country]:
-            data[country][date] = confirmed
-        else:
-            data[country][date] += confirmed
+    for row in rows:
+        if row[1] == "South Africa":
+            list_content = row[:-1]
+            
+    for i in list_content:
+        if i == "":
+            list_content.remove(i)
+    
+    south_africa = {"South Africa":list_content}
 
-# save the data as JSON file
-with open('covid19_data.json', 'w') as f:
-    json.dump(data, f)
+    # save the data as JSON file
+    with open('Covid19_data/covid19_data.json', 'w') as f:
+        json.dump(south_africa, f)
+
+gets_south_african_covid19_data()
